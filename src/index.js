2404bb182f1617ea915ad251401e863698d24eb9
Ansel Rognlie <36503186+anselrognlie@users.noreply.github.com>
2024-11-19 12:29:19 -0800

    updates entree key typo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-const entre = 'Fish Tacos';
+const entree = 'Fish Tacos';
 const price = 14.97;
 
 const order1 = {
-  entre: entre,
+  entree: entree,
   price: price,
 };
 
-const order2 = { entre, price };
+const order2 = { entree, price };
 
 console.log(`order1: ${JSON.stringify(order1)}`);
 console.log(`order2: ${JSON.stringify(order2)}`);
